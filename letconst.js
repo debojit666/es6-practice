606b7b6b51af81325394eac978d2846b81098a26
Debojit Das <debojit666@gmail.com>
2020-03-19 04:37:59 +0100

    tried multiline strings

---
+++ letconst.js
@@ -0,0 +1,23 @@
+const father = "Swapan Das";
+//console.log(father);
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(56);
+//console.log(numbers);
+const nayok = {name: "Sakib Khan", phone:45678};
+// konokichu const diye korle eta constant er moton kaaj korbe. 
+//ete push ba pop kora jaabe kintu shob kisu change kora jaabena
+
+
+//let use korle shei variable er maan change hoite parbe
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+
+console.log(patientName)
+
+//loop er bhetore let use korle sheta local variable hishabe kaaj korbe
+let sum = 0;
+for (let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
---
+++ template.js
@@ -0,0 +1,17 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " sings well.";
+const fullName2 = `${firstName} ${lastName} sings very well.`;
+console.log(fullName);
+console.log(fullName2);
+
+const multuLine = "Hello Hello Hello\n"
+    + "Is there anybody out there\n"
+    + "can you show me where it hurts?"
+console.log(multuLine)
+
+const multuLine2 = `There is no place
+you are receiding...
+there is no ships on the horizon.
+you are only going vains`
+console.log(multuLine2);

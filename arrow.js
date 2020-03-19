b5ed44832ea0d118e6300a13b01d6736b5c02c98
Debojit Das <debojit666@gmail.com>
2020-03-19 07:07:32 +0100

    tried single line arrow function

---
+++ arrow.js
@@ -0,0 +1,21 @@
+/**function doubleIt(num){
+    return num * 2;
+}
+const result = doubleIt(5);
+console.log(result);
+
+const double = function(num2){
+    return num2 * 2;
+}
+console.log(double); */
+
+
+const doubleIt = num => num * 2
+const result = doubleIt(5);
+console.log(result);
+
+
+
+const add =(x,y) => x + y;
+const summation = add(50,70);
+console.log(summation);

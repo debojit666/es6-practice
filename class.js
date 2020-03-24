20e5d67cad5e6539776ebf2846cef5a8e1a360f4
Debojit Das <debojit666@gmail.com>
2020-03-24 12:20:29 +0100

    inheritance, extends class, super

--- class.js
+++ class.js
@@ -10,6 +10,7 @@ const student1 = new Student(12, "Shuvo");
 const student2 = new Student(22, "Mahi");
 const student3 = new Student(16, "Salam");
 const student4 = new Student(17, "Mojnu");
+
 console.log(student1, student2, student3, student4);
 console.log(student1.name, student2.id, student3, student4);
 
---
+++ inheritance.js
@@ -0,0 +1,23 @@
+class Parent{
+    constructor(){
+        this.fatherName = "Arnold Schwerznegger";
+    }
+}
+
+class Child extends Parent{
+    constructor(name){
+        super();
+        this.name = name;
+    }
+}
+
+getFullName(){
+    return this.name + " " + this.fatherName;
+}
+
+const baby  = new Child("Jerry");
+const baby2 = new Child("Tom");
+console.log(baby);
+console.log(baby2);
+
+console.log(baby.getFullName());
